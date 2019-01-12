a2204263a8778f6e66f7b55a44d7d7107a993249
marios <Ouranos@marioss-MacBook-Pro.local>
2019-01-12 23:45:05 +0000

    add gitignore, create a new website folder - wip

---
+++ new/website/tmp/dev-server/react-static-browser-plugins.js
@@ -0,0 +1,12 @@
+// Imports
+
+
+// Plugins
+const plugins = [{
+  location: "/Users/Ouranos/Documents/github/MariosGeorgiou.github.io/new/website",
+  plugins: [],
+  hooks: {}
+}]
+
+// Export em!
+export default plugins
